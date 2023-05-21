329b77aaf861b7773e4555943dd638f24ce57004
tsubasa-alife <tsubasa.robotics@gmail.com>
2023-05-21 17:29:03 +0900

    第7章終了

---
+++ book4/ch07/q_learning_nn.py
@@ -0,0 +1,98 @@
+import sys
+sys.path.append("../common")
+import numpy as np
+import matplotlib.pyplot as plt
+from collections import defaultdict, deque
+from utils import greedy_probs, one_hot
+from gridworld import GridWorld
+from dezero import Model, optimizers
+import dezero.layers as L
+import dezero.functions as F
+
+
+class QNet(Model):
+    def __init__(self):
+        super().__init__()
+        self.l1 = L.Linear(100)
+        self.l2 = L.Linear(4)
+
+    def forward(self, x):
+        y = F.relu(self.l1(x))
+        y = self.l2(y)
+        return y
+
+
+class QLearningAgent:
+    def __init__(self):
+        self.gamma = 0.9
+        self.lr = 0.01
+        self.epsilon = 0.1
+        self.action_size = 4
+
+        self.qnet = QNet()
+        self.optimizer = optimizers.SGD(self.lr)
+        self.optimizer.setup(self.qnet)
+
+    def get_action(self, state_vec):
+        if np.random.rand() < self.epsilon:
+            return np.random.randint(0, self.action_size)
+        else:
+            qs = self.qnet(state_vec)
+            return qs.data.argmax()
+
+    def update(self, state, action, reward, next_state, done):
+        if done:
+            next_q = np.zeros(1)
+        else:
+            next_qs = self.qnet(next_state)
+            next_q = next_qs.max(axis=1)
+            next_q.unchain()
+        target = reward + self.gamma * next_q
+        qs = self.qnet(state)
+        q = qs[:, action]
+        loss = F.mean_squared_error(target, q)
+
+        self.qnet.cleargrads()
+        loss.backward()
+        self.optimizer.update()
+
+        return loss.data
+
+
+env = GridWorld()
+agent = QLearningAgent()
+
+episodes = 1000
+loss_history = []
+
+for episode in range(episodes):
+    state = env.reset()
+    state = one_hot(state)
+    total_loss, cnt = 0, 0
+    done = False
+
+    while not done:
+        action = agent.get_action(state)
+        next_state, reward, done = env.step(action)
+        next_state = one_hot(next_state)
+
+        loss = agent.update(state, action, reward, next_state, done)
+        total_loss += loss
+        cnt += 1
+        state = next_state
+
+    average_loss = total_loss / cnt
+    loss_history.append(average_loss)
+
+
+plt.xlabel('episode')
+plt.ylabel('loss')
+plt.plot(range(len(loss_history)), loss_history)
+plt.show()
+
+Q = {}
+for state in env.states():
+    for action in env.action_space:
+        q = agent.qnet(one_hot(state))[:, action]
+        Q[state, action] = float(q.data)
+env.render_q(Q)
